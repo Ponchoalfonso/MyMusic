3950c9bd0782506f2dd7fdf6cf96d997f9720019
5k <5k@iMac-de-iMac09.local>
2019-11-05 07:56:01 -0600

    Login view completed

--- src/LoginView.js
+++ src/LoginView.js
@@ -32,7 +32,7 @@ export default class LoginView extends Component {
 
     render() {
         return(
-            <View style={style.container}>
+            <View style={styles.container}>
                 <Image source={require('./assets/mymusic_logo.jpg')} style={styles.logo} resizeMode="contain" />
                 <TextInput
                     style={styles.textInput}
@@ -54,11 +54,56 @@ export default class LoginView extends Component {
                     returnKeyType={'done'}
                     onSubmitEditing={this.ingresar}
                 />
-                <TouchableOpacity onPress={this.ingresar} style={styles.boton} />
+                <TouchableOpacity onPress={this.ingresar} style={styles.boton}>
                 <Text style={styles.textoBoton}>
                     Log in
                 </Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: null,
+        height: null,
+        backgroundColor:'white',
+        alignItems: 'center',
+    },
+    logo:{
+        width: 200,
+        height: 150,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginTop: Platform.select({
+            ios: 90,
+            android: 50
+        }),
+        marginBottom: 25
+    },
+    textInput:{
+        height: 40,
+        backgroundColor: '#f2f2f2',
+        width: 230,
+        color: '#000035',
+        marginBottom: 40,
+        borderRadius: 5,
+        padding: 5,
+    },
+    boton:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 230,
+        height: 40,
+        backgroundColor: '#00ccd0',
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#fff'
+    },
+    textoBoton:{
+        color: '#fff',
+        fontSize: 22,
+    },
+});
